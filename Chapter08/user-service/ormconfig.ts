3e67647e6dd66be9185011691063fb969bd76417
MoonDongmin <20191836@o365.hanbat.ac.kr>
2023-06-29 18:38:37 +0900

    modified JWT

--- Chapter08/user-service/ormconfig.ts
+++ Chapter08/user-service/ormconfig.ts
@@ -2,14 +2,14 @@ import { DataSource } from 'typeorm'
 
 export const AppDataSource = new DataSource({
     type: 'mysql',
-    host: 'localhost',
+    host: '127.0.0.1',
     port: 3306,
     username: 'root',
     password: 'test',
     database: 'test',
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    entities: [__dirname+'/**/*.entity.js'],
     synchronize: false,
-    migrationsRun:false,
-    migrations: [__dirname + '/**/migrations/*.js'],
+    migrationsRun: true,
+    migrations: [__dirname + '/**/migrations/*.ts'],
     migrationsTableName: 'migrations',
 });
