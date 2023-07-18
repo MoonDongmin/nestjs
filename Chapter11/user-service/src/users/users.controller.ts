bcb4ce51ffc16c037c8f43cac1b4aa8c4d41a66e
MoonDongmin <20191836@o365.hanbat.ac.kr>
2023-07-18 17:46:32 +0900

    study Chap11

---
+++ Chapter11/user-service/src/users/users.controller.ts
@@ -0,0 +1,80 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Headers,
+  UseGuards,
+  Injectable,
+  Inject,
+  InternalServerErrorException,
+} from '@nestjs/common';
+//import { AuthGuard } from 'src/auth.guard';
+import { AuthService } from 'src/auth/auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserLoginDto } from './dto/user-login.dto';
+import { VerifyEmailDto } from './dto/verify-email.dto';
+import { UserInfo } from './UserInfo';
+import { UsersService } from './users.service';
+import { WINSTON_MODULE_NEST_PROVIDER, WinstonModule } from 'nest-winston';
+import { Logger as WinstonLogger } from 'winston';
+
+@Controller('users')
+export class UsersController {
+  constructor(
+    @Inject(WINSTON_MODULE_NEST_PROVIDER)
+    private readonly logger: WinstonLogger,
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
+
+  @Post()
+  async createUser(@Body() dto: CreateUserDto): Promise<void> {
+    this.printLoggerServiceLog(dto);
+    const { name, email, password } = dto;
+    await this.usersService.createUser(name, email, password);
+  }
+
+  private printLoggerServiceLog(dto) {
+    try {
+      throw new InternalServerErrorException('test');
+    } catch (e) {
+      this.logger.error('error: ' + JSON.stringify(dto), e.stack);
+    }
+    this.logger.warn('warn: ' + JSON.stringify(dto));
+    //this.logger.log('log: ' + JSON.stringify(dto));
+    this.logger.verbose('verbose: ' + JSON.stringify(dto));
+    this.logger.debug('debug: ' + JSON.stringify(dto));
+  }
+
+  @Post('/email-verify')
+  async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
+    const { signupVerifyToken } = dto;
+
+    return await this.usersService.verifyEmail(signupVerifyToken);
+  }
+
+  @Post('/login')
+  async login(@Body() dto: UserLoginDto): Promise<string> {
+    const { email, password } = dto;
+
+    return await this.usersService.login(email, password);
+  }
+
+  // @Get(':id')
+  // async getUserInfo(@Headers() headers: any, @Param('id') userId: string): Promise<UserInfo> {
+  //   const jwtString = headers.authorization.split('Bearer ')[1];
+  //
+  //   this.authService.verify(jwtString);
+  //
+  //   return this.usersService.getUserInfo(userId);
+  // }
+
+  // @UseGuards(AuthGuard)
+  // @Get(':id')
+  // async getUserInfo(@Headers() headers: any, @Param('id') userId: string): Promise<UserInfo> {
+  //   return this.usersService.getUserInfo(userId);
+  // }
+}
